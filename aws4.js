222173a381035785713ffa1d2f6e3fabcc52a3e0
Marcin Wiśniowski <shfx@shfx.pl>
2015-03-11 18:24:14 +0100

    Adding lowercase header check for x-amz-content-sha256

--- aws4.js
+++ aws4.js
@@ -80,7 +80,7 @@ RequestSigner.prototype.sign = function() {
   if (this.credentials.sessionToken)
     headers['X-Amz-Security-Token'] = this.credentials.sessionToken
 
-  if (this.service === 's3' && !headers['X-Amz-Content-Sha256'])
+  if (this.service === 's3' && !headers['X-Amz-Content-Sha256'] && !headers['x-amz-content-sha256'])
     headers['X-Amz-Content-Sha256'] = hash(this.request.body || '', 'hex')
 
   if (headers.Authorization) delete headers.Authorization
